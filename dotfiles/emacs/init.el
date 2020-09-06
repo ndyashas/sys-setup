41861a15bd43c6bd83cbf245a1b35b01ae0f60a9
Yashas ND <ndyashas@gmail.com>
2020-09-07 00:41:27 +0530

    Added emacs keybindings for resizing

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -105,3 +105,9 @@
 ;; (ivy-mode 1)
 
 (setq scheme-program-name "guile")
+
+;; Resizing window bindings
+(global-set-key (kbd "<C-up>") 'shrink-window)
+(global-set-key (kbd "<C-down>") 'enlarge-window)
+(global-set-key (kbd "<C-left>") 'shrink-window-horizontally)
+(global-set-key (kbd "<C-right>") 'enlarge-window-horizontally)
